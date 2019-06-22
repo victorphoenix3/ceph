5529a0afa3cd699854f11d1c85657cb9cb164350
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2019-06-22 10:41:45 +0530

    Added preprocessor WITH_JAEGER and jaeger configuration file.
    - added preprocessor option to build Ceph with/without Jaeger
    - added config.yml configuration file for Jaeger(for more info https://www.jaegertracing.io/docs/1.8/deployment/#configuration-options)

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -1569,7 +1569,6 @@ void PrimaryLogPG::do_request(
     //jaeger_ceph::setUpTracer();
     //auto span = opentracing::Tracer::Global()->StartSpan("traced_ms_fast_dispatch");
 
-
 /*
   void tracedSubroutine(const std::unique_ptr<opentracing::Span>& parentSpan)
   {

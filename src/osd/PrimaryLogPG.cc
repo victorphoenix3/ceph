b48c9a0c557187f2f050b61370934cefd785b26a
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2019-07-06 01:03:06 +0530

    fast test commit

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -1574,11 +1574,11 @@ void PrimaryLogPG::do_request(
     op->pg_trace.event("do request");
   }
 
-#ifdef WITH_JAEGER
-  JTracer jp;
-  jspan do_request_span = jp.tracedFunction("do_request_string");
-  do_request_span->Finish();
-#endif
+//#ifdef WITH_JAEGER
+//  JTracer jp;
+//  jspan do_request_span = jp.tracedFunction("do_request_string");
+//  do_request_span->Finish();
+//#endif
 
   // make sure we have a new enough map
   auto p = waiting_for_map.find(op->get_source());

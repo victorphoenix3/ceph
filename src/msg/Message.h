1ac82fac3a068fcdfdd194756673cce05d699a4d
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2019-07-19 02:53:34 +0530

    fast test commit

--- src/msg/Message.h
+++ src/msg/Message.h
@@ -252,8 +252,8 @@ public:
   void decode_trace(ceph::buffer::list::const_iterator &p, bool create = false, string t_meta);
 
 #ifdef WITH_JAEGER
-  encode_trace_jaeger(bufferlist & bl, uint64_t features);
-  decode_trace_jaeger(bufferlist::const_iterator& p, bool create, string t_meta);
+  string encode_trace_jaeger(bufferlist & bl, uint64_t features);
+  void decode_trace_jaeger(bufferlist::const_iterator& p, bool create, string t_meta);
 #endif
 
   class CompletionHook : public Context {

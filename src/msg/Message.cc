86f0aff52d96529d25ffe3af110145216614faf4
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2019-07-19 06:13:02 +0530

    fast test commit

--- src/msg/Message.cc
+++ src/msg/Message.cc
@@ -957,7 +957,7 @@ void Message::decode_trace(bufferlist::const_iterator &p, bool create)
 }
 
 #ifdef WITH_JAEGER
-  string Message::encode_trace_jaeger(
+  /*string*/ void Message::encode_trace_jaeger(
       bufferlist & bl, uint64_t features /*, jspan& parent_span*/) const {
     using ceph::encode;
 
@@ -974,7 +974,7 @@ void Message::decode_trace(bufferlist::const_iterator &p, bool create)
     encode(t_meta, bl);
     encode(*p, bl);
 
-    return t_meta;
+//    return t_meta;
   }
 
   void Message::decode_trace_jaeger(bufferlist::const_iterator & p, bool create) {

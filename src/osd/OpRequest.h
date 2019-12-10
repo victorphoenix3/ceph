d990c8f273730f819b767d91bdd60ae20825ba5a
Deepika Upadhyay <dupadhya@redhat.com>
2019-12-10 15:00:44 +0000

    error resolve in oprequest

--- src/osd/OpRequest.h
+++ src/osd/OpRequest.h
@@ -17,9 +17,7 @@
 #include "osd/osd_op_util.h"
 #include "osd/osd_types.h"
 #include "common/TrackedOp.h"
-#ifdef WITH_JAEGER
 #include "common/tracer.h"
-#endif
 
 /**
  * The OpRequest takes in a Message* and takes over a single reference

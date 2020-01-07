bc94df3a5609c57fe69ac84592ef6917745379f1
Deepika Upadhyay <dupadhya@redhat.com>
2020-01-07 14:54:15 +0530

    wip: Build Jaeger and it's dependencies as cmake ExternalProject it would be good idea to not expose all the jaeger variables to ceph.
    - add specific version of dependencies required
    	- OpenTracing: 1.5.x
    	- Jaeger: 0.5.0
    	- yaml-cpp:
    	- thrift: 0.11.0
    Adds:
    - cmake/modules/BuildJaeger.cmake
    - cmake/modules/BuildOpenTracing.cmake
    - cmake/modules/BuildThrift.cmake
    
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

---
+++ cmake/modules/Buildthrift.cmake
@@ -0,0 +1,30 @@
+function(build_thrift)
+  set(THRIFT_SOURCE_DIR "${CMAKE_SOURCE_DIR}/src/jaegertracing/thrift")
+  set(THRIFT_ROOT_DIR "${CMAKE_CURRENT_BINARY_DIR}/src/thrift")
+  set(THRIFT_INSTALL_DIR "${THRIFT_ROOT_DIR}/install")
+  set(THRIFT_BINARY_DIR "${THRIFT_ROOT_DIR}/build")
+
+  set(THRIFT_CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON)
+  list(APPEND THRIFT_CMAKE_ARGS -DBUILD_SHARED_LIBS=ON)
+  list(APPEND THRIFT_CMAKE_ARGS --DCMAKE_INSTALL_PREFIX=<THRIFT_INSTALL_DIR>)
+
+  if(CMAKE_MAKE_PROGRAM MATCHES "make")
+    # try to inherit command line arguments passed by parent "make" job
+    set(make_cmd $(MAKE) thrift)
+  else()
+    set(make_cmd ${CMAKE_COMMAND} --build <BINARY_DIR> --target thrift)
+  endif()
+
+
+  include(ExternalProject)
+  ExternalProject_Add(thrift
+    URL http://archive.apache.org/dist/thrift/0.11.0/thrift-0.11.0.tar.gz
+    DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/src
+    SOURCE_DIR ${THRIFT_SOURCE_DIR}
+    CONFIGURE_COMMAND ./bootstrap.sh && ./configure --prefix=<INSTALL_DIR>
+    CMAKE_ARGS ${THRIFT_CMAKE_ARGS}
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND ${make_cmd}
+    INSTALL_COMMAND make install
+    )
+endfunction()

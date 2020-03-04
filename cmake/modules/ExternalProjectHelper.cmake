ced303caaa9b35610fe8e05489f26f7510517ed5
Deepika Upadhyay <dupadhya@redhat.com>
2020-03-04 16:21:54 +0530

    test

--- cmake/modules/ExternalProjectHelper.cmake
+++ cmake/modules/ExternalProjectHelper.cmake
@@ -5,7 +5,7 @@ function (set_library_properties_for_external_project _target _lib)
   message(STATUS "Configuring ${_target} with ${_libpath}")
 
   file(MAKE_DIRECTORY "${_includepath}")
-  set_target_properties(TARGET ${_target} APPEND PROPERTY
+  set_target_properties(${_target} PROPERTIES
     INTERFACE_LINK_LIBRARIES "${_libpath}" "${CMAKE_DL_LIBS}"
     INTERFACE_INCLUDE_DIRECTORIES "${_includepath}"
     IMPORTED_LINK_INTERFACE_LANGUAGES "C"

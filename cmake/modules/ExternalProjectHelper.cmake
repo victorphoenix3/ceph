57611ce4b711769c74005213c1d5aed77b3f0f4f
Deepika Upadhyay <dupadhya@redhat.com>
2020-03-04 16:17:04 +0530

    only fix-interfacing

--- cmake/modules/ExternalProjectHelper.cmake
+++ cmake/modules/ExternalProjectHelper.cmake
@@ -4,17 +4,15 @@ function (set_library_properties_for_external_project _target _lib)
   set(_includepath "${CMAKE_BINARY_DIR}/external/include")
   message(STATUS "Configuring ${_target} with ${_libpath}")
 
-  set_property(TARGET ${_target} APPEND PROPERTY
+  file(MAKE_DIRECTORY "${_includepath}")
+  set_target_properties(TARGET ${_target} APPEND PROPERTY
     INTERFACE_LINK_LIBRARIES "${_libpath}" "${CMAKE_DL_LIBS}"
-    INTERFACE_INCLUDE_DIRECTORIES "${OpenTracing_INCLUDE_DIRS}"
-    IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-    IMPORTED_LOCATION "${_libpath}")
+    INTERFACE_INCLUDE_DIRECTORIES "${_includepath}"
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${OpenTracing_LIBRARIES}")
   # Manually create the directory, it will be created as part of the build,
   # but this runs in the configuration phase, and CMake generates an error if
   # we add an include directory that does not exist yet.
-  file(MAKE_DIRECTORY "${_includepath}")
-  set_property(TARGET ${_target} APPEND PROPERTY
-	  INTERFACE_INCLUDE_DIRECTORIES "${_includepath}")
 endfunction ()
 
 function (set_executable_name_for_external_project VAR _exe)
--- cmake/modules/IncludeJaeger.cmake
+++ cmake/modules/IncludeJaeger.cmake
@@ -13,6 +13,3 @@ add_dependencies(opentracing::libopentracing OpenTracing)
 set_library_properties_for_external_project(opentracing::libopentracing
   opentracing)
 #set_library_properties_for_external_project(opentracing::libopentracing yaml-cpp)
-set_property(TARGET opentracing::libopentracing APPEND PROPERTY
-  INTERFACE_LINK_LIBRARIES opentracing::libopentracing
-  INTERFACE_LINK_LIBRARIES "${CMAKE_DL_LIBS}")
